110483b62b8f8727f97fd186705be36fee4a87f3
laurix <grossoanalaura@gmail.com>
2023-12-05 17:40:48 -0300

    falta testear pero creo que un poco funciona, faltan controles

--- 20231205-DOM/js/calculadora.js
+++ 20231205-DOM/js/calculadora.js
@@ -14,3 +14,44 @@ let suma = document.getElementById("btnSuma");
 let resta= document.getElementById("btnResta");
 let multip = document.getElementById("btnMult");
 let dividir = document.getElementById("btnDiv");
+let igual = document.getElementById("btnResult");
+let pantalla = document.getElementById("h3Pantalla");
+let escritura ="";
+let operacion;
+const btnNumero = document.getElementsByClassName("btnNum");
+
+
+uno.addEventListener("click", ()=> addEscritura("1")) ;
+dos.addEventListener("click", ()=> addEscritura("2")) ;
+tres.addEventListener("click", ()=> addEscritura("3")) ;
+cuatro.addEventListener("click", ()=> addEscritura("4")) ;
+cinco.addEventListener("click", ()=> addEscritura("5")) ;
+seis.addEventListener("click", ()=> addEscritura("6")) ;
+siete.addEventListener("click", ()=> addEscritura("7")) ;
+ocho.addEventListener("click", ()=> addEscritura("8")) ;
+nueve.addEventListener("click", ()=> addEscritura("9")) ;
+cero.addEventListener("click", ()=> addEscritura("0")) ;
+suma.addEventListener("click", ()=> tomarDatos("+"));
+resta.addEventListener("click", ()=> tomarDatos("-"));
+dividir.addEventListener("click", ()=> tomarDatos("/"));
+multip.addEventListener("click", ()=> tomarDatos("*"));
+igual.addEventListener("click", ()=> resultado() )
+
+let addEscritura = (txt) =>{
+    escritura += txt;
+    screen(escritura);
+}
+
+const tomarDatos = (operando) => {
+    calculo = parseInt(escritura);
+    escritura = "";
+    operacion = operando;
+};
+
+const resultado = () => {
+    
+}
+
+
+const screen = (texto) =>  pantalla.innerText = texto;
+
