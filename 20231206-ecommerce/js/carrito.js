ba991dc090efdffcc7bade7dc43ea712f2284ebd
laurix <grossoanalaura@gmail.com>
2023-12-06 17:17:06 -0300

    mejorando funcionamiento, levanta storage y muestra en carrito

--- 20231206-ecommerce/js/carrito.js
+++ 20231206-ecommerce/js/carrito.js
@@ -2,6 +2,10 @@ const cantProductos = localStorage.length;
 let arrClaves = [];
 let arrPrecios = [];
 let total = 0;
+const listadoDetalle = document.getElementById("divProd");
+const listadoPrecios = document.getElementById("divPrecio");
+let liDetalle = document.createElement("li");
+let liPrecio = document.createElement("li");
 
 //buscar claves si hay
 const traerKeys = (cant) =>{
@@ -17,12 +21,21 @@ if(cantProductos>0){
     traerKeys(cantProductos);
     total = armarPrecios(arrClaves);
 
+    insertarUl();
 }else{
 
 }
 
 
-
+function insertarUl(){
+    for(let i =0; i<cantProductos;i++){
+        
+        liDetalle.innerText= arrClaves[i];
+        listadoDetalle.appendChild(liDetalle);
+        liPrecio.innerText= arrPrecios[i];
+        listadoPrecios.appendChild(liPrecio);
+    }
+}
 
 function armarPrecios(arrClaves){
     let suma = 0;
@@ -35,3 +48,4 @@ function armarPrecios(arrClaves){
     return suma;
 
 }
+
