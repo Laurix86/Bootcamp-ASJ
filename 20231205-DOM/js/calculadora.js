860bf79336ea51a21741106e16a0d0e3f3aa1e4f
laurix <grossoanalaura@gmail.com>
2023-12-06 10:18:51 -0300

    mejoras en calculadora, dark mode en proceso

--- 20231205-DOM/js/calculadora.js
+++ 20231205-DOM/js/calculadora.js
@@ -1,4 +1,5 @@
 let calculo = 0;
+const html = document.getElementById("html");
 const uno = document.getElementById("btn1");
 const dos = document.getElementById("btn2");
 const tres = document.getElementById("btn3");
@@ -19,8 +20,17 @@ const pantalla = document.getElementById("h3Pantalla");
 let escritura ="";
 let operacion;
 const btnNumero = document.getElementsByClassName("btnNum");
+const btnOperacion = document.getElementsByClassName("btnOp");
+const modo = document.getElementById("flexSwitchCheckReverse");
 
+modo.addEventListener("click", function(e){
+    if(!e.target.checked){
+        html.data-bs-theme = "dark";
+        html.setAttribute("data-bs-theme", html.getAttribute("data-bs-theme"));
+    }
+});
 
+/* Reemplazado abajo con e.target
 uno.addEventListener("click", ()=> addEscritura("1")) ;
 dos.addEventListener("click", ()=> addEscritura("2")) ;
 tres.addEventListener("click", ()=> addEscritura("3")) ;
@@ -30,14 +40,19 @@ seis.addEventListener("click", ()=> addEscritura("6")) ;
 siete.addEventListener("click", ()=> addEscritura("7")) ;
 ocho.addEventListener("click", ()=> addEscritura("8")) ;
 nueve.addEventListener("click", ()=> addEscritura("9")) ;
-cero.addEventListener("click", ()=> addEscritura("0")) ;
+cero.addEventListener("click", ()=> addEscritura("0")) ; 
+*/
+/* también reemplazado abajo con e.target
 suma.addEventListener("click", ()=> tomarDatos("+"));
 resta.addEventListener("click", ()=> tomarDatos("-"));
 dividir.addEventListener("click", ()=> tomarDatos("/"));
-multip.addEventListener("click", ()=> tomarDatos("*"));
+multip.addEventListener("click", ()=> tomarDatos("*")); 
+*/
 igual.addEventListener("click", ()=> resultado() );
 borrar.addEventListener("click", ()=> borrarTodo());
 
+
+    
 
 let addEscritura = (txt) =>{
     escritura += txt;
@@ -59,23 +74,40 @@ const resultado = () => {
     switch(operacion){
         case "+":
             calculo += parseInt(escritura);
-            screen(calculo);
+            screenResultado(calculo);
             break;
         case "-":
             calculo -= parseInt(escritura);
-            screen(calculo);
+            screenResultado(calculo);
             break;
         case "/":
             calculo /= parseInt(escritura);
-            screen(calculo);
+            screenResultado(calculo);
             break;
         case "*":
             calculo *= parseInt(escritura);
-            screen(calculo);
+            screenResultado(calculo);
             break;
     }
+
 }
 
 
 const screen = (texto) =>  pantalla.innerText = texto;
 
+const screenResultado = (texto) => {
+    pantalla.innerText = " = " + texto;
+    pantalla.style.alignContent = "flex-end";
+}
+
+for(let btnN of btnNumero){
+    btnN.addEventListener("click", function(e){
+        addEscritura(e.target.value);
+    }
+)};
+
+for(let btnO of btnOperacion){
+    btnO.addEventListener("click", function(e){
+        tomarDatos(btnO.value);
+    })
+};
