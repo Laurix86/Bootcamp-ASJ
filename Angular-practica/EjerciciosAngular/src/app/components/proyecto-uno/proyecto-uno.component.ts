55c9d723e68712447a94e3618b016362e6cddc18
laurix <grossoanalaura@gmail.com>
2023-12-13 17:20:36 -0300

    agrega tareas en proyecto 1, falta eliminar/tachar

--- Angular-practica/EjerciciosAngular/src/app/components/proyecto-uno/proyecto-uno.component.ts
+++ Angular-practica/EjerciciosAngular/src/app/components/proyecto-uno/proyecto-uno.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { text } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-proyecto-uno',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./proyecto-uno.component.css']
 })
 export class ProyectoUnoComponent {
+  input_text = "";
+  lista: string [] = [];
+  
+  agregarTarea(){
+    this.lista.push(this.input_text.toUpperCase());
+    this.input_text = "";
+  };
+  
 
 }
